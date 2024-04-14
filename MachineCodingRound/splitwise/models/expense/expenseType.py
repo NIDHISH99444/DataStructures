9259f2a22b838f2f43f2e3680d3b6832c5a42bfe
nidhish28 <nidhish.singhal@cashflo.io>
2024-04-14 10:49:38 +0530

    modifying files

--- MachineCodingRound/splitwise/models/expense/expenseType.py
+++
@@ -1,6 +0,0 @@
-from enum import Enum
-
-class ExpenseType(Enum):
-    EQUAL = 1
-    EXACT = 2
-    PERCENT = 3
--- MachineCodingRound/splitwise/models/split/equalSplit.py
+++
@@ -1,6 +0,0 @@
-from splitwise.models.split.split import Split
-
-
-class EqualSplit(Split):
-    def __init__(self, user):
-        super().__init__(user)
